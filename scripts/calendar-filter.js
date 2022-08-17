8891e93077663ede08936ffa621252b6f54842dd
rohandharwadker <rohan.dharwadker@gmail.com>
2022-08-16 21:31:18 -0500

    [#047] add download pdf button to calendar

--- scripts/calendar-filter.js
+++ scripts/calendar-filter.js
@@ -1,56 +1,68 @@
 function calendarFilter(c) {
-    var x, i;
-    x = document.getElementsByClassName("calendar-event"); // all filter items class
-    if (c == "all") c = "";
-    var filtered = 0;
-    for (i = 0; i < x.length; i++) {
-      filterRemoveClass(x[i], "calendar-filter-show");
-      if (x[i].className.indexOf(c) > -1){
-          filterAddClass(x[i], "calendar-filter-show");
-          filtered++
-      }
-    }
-    var header = document.getElementById("calendar-filter-head");
-    var headerVar = "";
-    if (c == '') headerVar = "All";
-    else if (c == 'calendar-filter-mandatory') headerVar = "Mandatory";
-    else if (c == 'calendar-filter-social') headerVar = "Social";
-    else headerVar = "Extra-Curricular";
-    header.innerHTML = headerVar+" Events ("+filtered+")";
-  }
-  
-  // Show filtered elements
-  function filterAddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
+  var x, i;
+  x = document.getElementsByClassName("calendar-event"); // all filter items class
+  if (c == "all") c = "";
+  var filtered = 0;
+  for (i = 0; i < x.length; i++) {
+    filterRemoveClass(x[i], "calendar-filter-show");
+    if (x[i].className.indexOf(c) > -1){
+        filterAddClass(x[i], "calendar-filter-show");
+        filtered++
     }
   }
-  
-  // Hide elements that are not selected
-  function filterRemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
+  var header = document.getElementById("calendar-filter-head");
+  var headerVar = "";
+  if (c == '') headerVar = "All";
+  else if (c == 'calendar-filter-mandatory') headerVar = "Mandatory";
+  else if (c == 'calendar-filter-social') headerVar = "Social";
+  else headerVar = "Extra-Curricular";
+  header.innerHTML = headerVar+" Events ("+filtered+")";
+}
+
+// Show filtered elements
+function filterAddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
-    element.className = arr1.join(" ");
   }
-  
-  // Add active class to the current control button (highlight it)
-  var btnContainer = document.getElementById("calendar-filter-container");
-  var btns = btnContainer.getElementsByClassName("calendar-filter-button");
-  for (var i = 0; i < btns.length; i++) {
+}
+
+// Hide elements that are not selected
+function filterRemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Download Calendar
+function downloadCalendar() {
+  const a = document.createElement('a')
+  a.href = "files/Calendar22-23.pdf"
+  a.download = "files/Calendar22-23.pdf".split('/').pop()
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("calendar-filter-container");
+var btns = btnContainer.getElementsByClassName("calendar-filter-button");
+for (var i = 0; i < btns.length; i++) {
+  if (btns[i].innerHTML != "Download PDF"){
     btns[i].addEventListener("click", function() {
       var current = document.getElementsByClassName("calendar-filter-button-active");
       current[0].className = current[0].className.replace(" calendar-filter-button-active", "");
       this.className += " calendar-filter-button-active";
     });
   }
+}
